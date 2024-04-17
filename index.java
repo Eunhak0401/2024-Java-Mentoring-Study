07273a29741078d21832731acb25c6b3a5341e9b
Eunhak0401 <74fresh4490@gmail.com>
2024-04-18 00:23:29 +0900

    int 와 float

---
+++ index.java
@@ -0,0 +1,13 @@
+public class index {
+    public static void main(String[] args) {
+        int a;
+
+        double c;
+        a = 3;
+
+        c = 0.1;
+        System.out.println(a);
+        
+        System.out.println(c);
+    }
+}
