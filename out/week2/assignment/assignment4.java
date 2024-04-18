31c67ab1ef8c6578155ddf29288e907b43f04477
Eunhak0401 <74fresh4490@gmail.com>
2024-04-19 00:21:12 +0900

    과제4 제출

--- out/week2/assignment/assignment4.java
+++ out/week2/assignment/assignment4.java
@@ -1,4 +1,16 @@
 package assignment;
 
 public class assignment4 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println(a);
+        System.out.println(b);
+
+    }
 }
