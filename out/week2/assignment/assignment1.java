4179b3ea4f7019e6409bbfc7ca83cbacf3844b05
Eunhak0401 <74fresh4490@gmail.com>
2024-04-19 00:02:01 +0900

    Revert "no message"
    This reverts commit 248cef5c02996fb59c7e976fa703776b467fb2d6.

--- out/week2/assignment/assignment1.java
+++ out/week2/assignment/assignment1.java
@@ -1,13 +1,4 @@
 package assignment;
 
-//본인의 이름과 학년을 변수에 저장하여 출력하는 코드를 작성하시오. 대신, 각각 알맞는 자료형을 사용하여 출력할 것
-
 public class assignment1 {
-    public static void main(String[] args) {
-        String myname = "이은학";
-        int myGrade = 1;
-
-        System.out.println(myname);
-        System.out.println(myGrade);
-    }
 }
