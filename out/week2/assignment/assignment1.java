70344c03b39327022e057049d6f4b120f4a881aa
Eunhak0401 <74fresh4490@gmail.com>
2024-04-19 00:05:26 +0900

    과제 1 제출

--- out/week2/assignment/assignment1.java
+++ out/week2/assignment/assignment1.java
@@ -1,4 +1,11 @@
 package assignment;
 
 public class assignment1 {
+    public static void main(String[] args) {
+        String myName = "이은학";
+        int myGrade = 1;
+
+        System.out.println(myName);
+        System.out.println(myGrade);
+    }
 }
