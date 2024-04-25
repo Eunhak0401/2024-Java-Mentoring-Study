62b608c5ded0d14a49226eb04ed414e6aa57aecb
Eunhak0401 <74fresh4490@gmail.com>
2024-04-25 17:13:42 +0900

    week3

---
+++ out/week2/week3_1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class week3_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        byte a = sc.nextByte();
+        short b = sc.nextShort();
+
+        float c = sc.nextFloat();
+
+        boolean d = sc.nextBoolean();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
