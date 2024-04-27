4c49660f979a944272e748dfb6cd4ca9e0fde9d0
Eunhak0401 <74fresh4490@gmail.com>
2024-04-28 01:44:08 +0900

    과제 2까지 완료

--- src/src/week3/Assignment1.java
+++ src/src/week3/Assignment1.java
@@ -1,2 +1,28 @@
-package week3;public class Assignment1 {
+package week3;
+
+import java.util.Scanner;
+
+public class Assignment1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A, B;
+
+        System.out.println("A를 입력하시오:");
+        int num = scanner.nextInt();
+        System.out.println("B를 입력하시오:");
+        int num1 = scanner.nextInt();
+
+        if (num > num1) {
+            System.out.println(">");
+        }
+        if (num < num1) {
+            System.out.println("<");
+        }
+        if (num == num1) {
+            System.out.println("==");
+        }
+
+
+
+    }
 }
