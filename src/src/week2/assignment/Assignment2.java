d4002eac6cafb9f1821236f1e3d49fe34b6b2dea
Eunhak0401 <74fresh4490@gmail.com>
2024-05-01 23:29:06 +0900

    4주차 생성

--- src/src/week2/assignment/Assignment2.java
+++
@@ -1,9 +0,0 @@
-package assignment;
-
-public class Assignment2 {
-    public static void main(String[] args) {
-        double num = 39835.2311;
-
-        System.out.println((int)num);
-    }
-}
--- src/src/week2/assignment/Assignment3.java
+++
@@ -1,9 +0,0 @@
-package assignment;
-
-public class Assignment3 {
-    public static void main(String[] args) {
-        int a = 19;
-
-        System.out.println(a);
-    }
-}
