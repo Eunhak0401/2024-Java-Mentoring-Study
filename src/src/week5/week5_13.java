cf8b497388403cae77fb1cd9cb2772c9ef7a3b30
Eunhak0401 <74fresh4490@gmail.com>
2024-05-19 23:56:33 +0900

    5주차 마지막 문제 풀이

--- src/src/week5/week5_13.java
+++ src/src/week5/week5_13.java
@@ -6,19 +6,45 @@ public class week5_13 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("몇개의 정수를 입력하시겠습니까? : ");
         int num = scanner.nextInt();
-        int[] array = new int[num];
 
-        if (num >= 0){
+        if (num > 0) {
+            int[] array = new int[num];
 
+            System.out.println("정수" + num + "개 입력: ");
+
+            for (int i = 0; i < num; i++) {
+                array[i] = scanner.nextInt();
+                }
+
+            int min = array[0];
+            int max = array[0];
+
+            for (int i = 1; i < num; i++) {
+                if (array[i] < min) {
+                    min = array[i];
+                }
+                if (array[i] > max) {
+                    max = array[i];
+                }
+            }
+
+            System.out.println("입력된 정수는 다음과 같습니다: ");
+            for (int a : array) {
+                System.out.print(a + " ");
+            }
+            System.out.println(); // 개행
+
+            // 최소값과 최댓값 출력
+            System.out.println("최소값: " + min);
+            System.out.println("최댓값: " + max);
         }
-        if(array[i] > max) {
-            max = array[i];
-        } else {
-            min = array[i];
-        }
+        scanner.close(); // Scanner 객체를 닫음
     }
 }
+
+
 
 
 
